891183f9ee60192609369b0f53ec538ec30f46b4
Kaito Kitaya <kitaya@papyrusde.com>
2025-11-27 08:57:08 +0100

    add: add new article

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75938c92b0f0e839"; }
+/******/ 		__webpack_require__.h = function() { return "b698c86c5e645d6e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
